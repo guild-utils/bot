c829a9a117703eeb6acbfc723d96b5febd5e8b61
tignear <tignear+m@gmail.com>
2020-05-12 03:47:56 +0900

    fix bug

--- packages/presentation/klasa/main.ts
+++ packages/presentation/klasa/main.ts
@@ -20,7 +20,6 @@ class Client extends KlasaClient {
 	constructor(options: KlasaClientOptions) {
 		super(options);
 		gameEventNotificationRepository.init(this);
-		initChannelsGateway(this.gateways);
 		// Add any properties to your Klasa Client
 	}
 
@@ -48,4 +47,6 @@ KlasaClient.defaultGuildSchema.add('counter',f=>{
 container.register("GameEventUseCase",{useValue:usecase});
 container.register("GameEventNotificationRepository",{useValue:gameEventNotificationRepository});
 const client =new Client(config);
+initChannelsGateway(client.gateways);
+
 client.login(token);
