c08af7b69fdbaeda1ee5fa3c7214538d17ff90c4
tignear <tignear+m@gmail.com>
2020-07-01 17:10:10 +0900

    update

---
+++ packages/presentation/klasa-member-gateway/src/lib/extensions/KlasaGuild.ts
@@ -0,0 +1,15 @@
+import { Structures } from 'discord.js';
+
+Structures.extend('Guild', Guild => {
+	/**
+	 * Mutates KlasaGuild to include a KlasaMemberStore with our extensions
+	 * @extends external:Guild
+	 */
+	return class  extends Guild {
+		constructor(client, data) {
+			// avoid double iteration by the super class populating the members collection
+			const { members, ...restData } = data || {};
+			super(client, Object.keys(restData).length ? restData : undefined);
+		}
+	};
+});
