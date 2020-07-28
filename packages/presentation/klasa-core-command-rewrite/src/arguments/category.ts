6b5a3c2551ff3063e13b0117d14ddcd8c920298e
tignear <tignear+m@gmail.com>
2020-07-28 18:45:24 +0900

    fix bug at deep path

--- packages/presentation/klasa-core-command-rewrite/src/arguments/category.ts
+++ packages/presentation/klasa-core-command-rewrite/src/arguments/category.ts
@@ -30,6 +30,9 @@ export default class extends Argument {
     const categorized = categorizeCommand(this.client.commands);
     if (split.length == 2) {
       const category = categorized[split[0].toLowerCase()];
+      if (!category) {
+        return undefined;
+      }
       return (
         category.subCategory[split[1].toLowerCase()] ??
         category.direct.find(
@@ -38,7 +41,28 @@ export default class extends Argument {
             cmd.aliases
               .map((e) => e.toLowerCase())
               .includes(split[1].toLowerCase())
-        )
+        ) ??
+        category
+      );
+    }
+    if (split.length === 3) {
+      const categorized = categorizeCommand(this.client.commands);
+      const category = categorized[split[0].toLowerCase()];
+      if (!category) {
+        return undefined;
+      }
+      const subCategory = category.subCategory[split[1].toLowerCase()];
+      if (!subCategory) {
+        return category;
+      }
+      return (
+        subCategory.command.find(
+          (cmd) =>
+            cmd.name.toLowerCase() === split[1].toLowerCase() ||
+            cmd.aliases
+              .map((e) => e.toLowerCase())
+              .includes(split[2].toLowerCase())
+        ) ?? subCategory
       );
     }
     return categorized[arg.toLowerCase()];
