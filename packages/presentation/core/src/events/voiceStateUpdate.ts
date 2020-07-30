ce204ebcfa4722ca03ff3b53a170c2b6bfcc0766
tignear <tignear+m@gmail.com>
2020-07-30 16:53:41 +0900

    fix bug

--- packages/presentation/core/src/events/voiceStateUpdate.ts
+++ packages/presentation/core/src/events/voiceStateUpdate.ts
@@ -1,21 +1,35 @@
 import { Event, EventStore } from "klasa";
 import { VoiceState } from "discord.js";
+import { inject, autoInjectable } from "tsyringe";
+import Engine from "../text2speech/engine";
+import { text2speechTargetTextChannels } from "../guild_settings_keys";
 
+@autoInjectable()
 export default class extends Event {
-  constructor(store: EventStore, file: string[], directory: string) {
+  constructor(
+    store: EventStore,
+    file: string[],
+    directory: string,
+    @inject("engine") private readonly engine: Engine
+  ) {
     super(store, file, directory, {
       event: "voiceStateUpdate",
     });
   }
   // eslint-disable-next-line @typescript-eslint/no-unused-vars
-  run(oldState: VoiceState, _newState: VoiceState): void {
+  async run(oldState: VoiceState, _newState: VoiceState): Promise<void> {
     const oldChannel = oldState.channel;
     const inHuman = oldChannel?.members.some(
       (e) => !e.user.bot && e.user.id !== oldState.member?.user.id
     );
     if (inHuman) {
-      return
+      return;
     }
+    await this.engine.unregister(oldChannel?.guild!.voice?.connection);
+    oldChannel?.guild!.voice?.connection?.disconnect();
+    await oldChannel?.guild.settings.reset(
+      text2speechTargetTextChannels.join(".")
+    );
     oldChannel?.leave();
   }
 }
