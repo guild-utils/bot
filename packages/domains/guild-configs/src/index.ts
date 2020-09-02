ba8aa5169066350025ae2f7f60839e25f96edbac
tignear <tignear+m@gmail.com>
2020-09-02 13:33:33 +0900

    [add][domain] add guild-configs

---
+++ packages/domains/guild-configs/src/index.ts
@@ -0,0 +1,32 @@
+export const languages = {
+  ja_JP: "ja_JP",
+};
+export type BasicBotConfig = {
+  prefix: string;
+  language: keyof typeof languages;
+  disabledCommands: Set<string>;
+};
+export type UpdateResult<T, U = T> = {
+  type: "ok" | "same" | "error";
+  before: T;
+  after: U;
+};
+export interface BasicBotConfigRepository {
+  getPrefix(guild: string): Promise<string>;
+  setPrefix(guild: string, prefix: string): Promise<UpdateResult<string>>;
+  getLanguage(guild: string): Promise<keyof typeof languages>;
+  setLanguage(
+    guild: string,
+    language: keyof typeof languages
+  ): Promise<UpdateResult<keyof typeof languages>>;
+  getDisabledCommands(guild: string): Promise<Set<string>>;
+  addDisabledCommands(
+    guild: string,
+    key: string
+  ): Promise<UpdateResult<Set<string>>>;
+  removeDisabledCommands(
+    guild: string,
+    key: string
+  ): Promise<UpdateResult<Set<string>>>;
+  get(guild: string): Promise<BasicBotConfig>;
+}
