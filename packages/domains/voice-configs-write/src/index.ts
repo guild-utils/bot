33f5aacac4126169c85e0e1d6c16daed203e741e
tignear <tignear+m@gmail.com>
2020-10-12 00:23:10 +0900

    [fix][domain] use meta and accept undefined

--- packages/domains/voice-configs-write/src/index.ts
+++ packages/domains/voice-configs-write/src/index.ts
@@ -1,27 +1,10 @@
+import type { UpdateResult, VoiceKindType } from "domain_meta";
+export type { UpdateResult, VoiceKindType };
 export const Randomizer = {
   v1: "v1",
   v2: "v2",
 };
-export const VoiceKind = {
-  normal: "normal",
-  angry: "angry",
-  happy: "happy",
-  neutral: "neutral",
-  sad: "sad",
-  mei_angry: "mei_angry",
-  mei_bashful: "mei_bashful",
-  mei_happy: "mei_happy",
-  mei_normal: "mei_normal",
-  mei_sad: "mei_sad",
-  takumi_angry: "takumi_angry",
-  takumi_happy: "takumi_happy",
-  takumi_normal: "takumi_normal",
-  takumi_sad: "takumi_sad",
-  alpha: "alpha",
-  beta: "beta",
-  gamma: "gamma",
-  delta: "delta",
-};
+
 export type LayeredVoiceConfig = {
   allpass: number | undefined;
   intone: number | undefined;
@@ -29,39 +12,53 @@ export type LayeredVoiceConfig = {
   threshold: number | undefined;
   tone: number | undefined;
   volume: number | undefined;
-  kind: keyof typeof VoiceKind | undefined;
+  kind: VoiceKindType | undefined;
   randomizer: keyof typeof Randomizer | undefined;
   readName: string | undefined;
 };
-export type UpdateResult<T, U = T> = {
-  type: "ok" | "same" | "error" | "not matched";
-  before?: T | undefined;
-  after?: U | undefined;
-};
+
 export interface LayeredVoiceConfigRepository<LayerKeyType> {
-  get(layerKey: LayerKeyType): Promise<LayeredVoiceConfig>;
+  get(layerKey: LayerKeyType): Promise<LayeredVoiceConfig | undefined>;
   set(
     layerKey: LayerKeyType,
     newC: LayeredVoiceConfig
   ): Promise<UpdateResult<LayeredVoiceConfig>>;
-  setAllpass(layerKey: LayerKeyType, v: number): Promise<UpdateResult<number>>;
-  setIntone(layerKey: LayerKeyType, v: number): Promise<UpdateResult<number>>;
-  setSpeed(layerKey: LayerKeyType, v: number): Promise<UpdateResult<number>>;
+  setAllpass(
+    layerKey: LayerKeyType,
+    v: number | undefined
+  ): Promise<UpdateResult<number | undefined>>;
+  setIntone(
+    layerKey: LayerKeyType,
+    v: number | undefined
+  ): Promise<UpdateResult<number | undefined>>;
+  setSpeed(
+    layerKey: LayerKeyType,
+    v: number | undefined
+  ): Promise<UpdateResult<number | undefined>>;
   setThreshold(
     layerKey: LayerKeyType,
-    v: number
-  ): Promise<UpdateResult<number>>;
-  setTone(layerKey: LayerKeyType, v: number): Promise<UpdateResult<number>>;
-  setVolume(layerKey: LayerKeyType, v: number): Promise<UpdateResult<number>>;
+    v: number | undefined
+  ): Promise<UpdateResult<number | undefined>>;
+  setTone(
+    layerKey: LayerKeyType,
+    v: number | undefined
+  ): Promise<UpdateResult<number | undefined>>;
+  setVolume(
+    layerKey: LayerKeyType,
+    v: number | undefined
+  ): Promise<UpdateResult<number | undefined>>;
   setKind(
     layerKey: LayerKeyType,
-    v: keyof typeof VoiceKind
-  ): Promise<UpdateResult<keyof typeof VoiceKind>>;
+    v: VoiceKindType | undefined
+  ): Promise<UpdateResult<VoiceKindType | undefined>>;
   setRandomizer(
     layerKey: LayerKeyType,
-    v: keyof typeof Randomizer
-  ): Promise<UpdateResult<keyof typeof Randomizer>>;
-  setReadName(layerKey: LayerKeyType, v: string): Promise<UpdateResult<string>>;
+    v: keyof typeof Randomizer | undefined
+  ): Promise<UpdateResult<keyof typeof Randomizer | undefined>>;
+  setReadName(
+    layerKey: LayerKeyType,
+    v: string | undefined
+  ): Promise<UpdateResult<string | undefined>>;
   setIfNotChanged(
     layerKey: LayerKeyType,
     newC: LayeredVoiceConfig,
@@ -75,18 +72,27 @@ export type GuildVoiceConfig = {
   randomizer?: keyof typeof Randomizer;
 };
 export interface GuildVoiceConfigRepository {
-  get(guild: string): Promise<GuildVoiceConfig>;
+  get(guild: string): Promise<GuildVoiceConfig | undefined>;
   set(
     guild: string,
     newC: GuildVoiceConfig
   ): Promise<UpdateResult<GuildVoiceConfig>>;
-  setReadName(guild: string, v: boolean): Promise<UpdateResult<boolean>>;
-  setMaxReadLimit(guild: string, v: number): Promise<UpdateResult<number>>;
-  setMaxVolume(guild: string, v: number): Promise<UpdateResult<number>>;
+  setReadName(
+    guild: string,
+    v: boolean | undefined
+  ): Promise<UpdateResult<boolean | undefined>>;
+  setMaxReadLimit(
+    guild: string,
+    v: number | undefined
+  ): Promise<UpdateResult<number | undefined>>;
+  setMaxVolume(
+    guild: string,
+    v: number | undefined
+  ): Promise<UpdateResult<number | undefined>>;
   setRandomizer(
     guild: string,
-    v: keyof typeof Randomizer
-  ): Promise<UpdateResult<keyof typeof Randomizer>>;
+    v: keyof typeof Randomizer | undefined
+  ): Promise<UpdateResult<keyof typeof Randomizer | undefined>>;
   setIfNotChanged(
     guild: string,
     newC: GuildVoiceConfig,
