ba4f09d0ef6b2abd53a484ec00fd33eb46c06f7d
tignear <tignear+m@gmail.com>
2020-10-12 00:22:02 +0900

    [fix][util] use guildConfigRepository

--- packages/util/klasa/src/inhibitors/disabled.js
+++ packages/util/klasa/src/inhibitors/disabled.js
@@ -5,6 +5,9 @@ module.exports = class extends Inhibitor {
 	run(message, command) {
 		if (!command.enabled) throw message.language.get('INHIBITOR_DISABLED_GLOBAL');
 		const set = this.client.options.guildConfigRepository.getDisabledCommands(message);
+		if (!set) {
+			return;
+		}
 		if (set.has(command.name)) throw message.language.get('INHIBITOR_DISABLED_GUILD');
 	}
 
