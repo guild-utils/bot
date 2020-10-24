89bc71e09b9484889f3173967016e0c54c6400bc
tignear <tignear+m@gmail.com>
2020-10-24 16:12:20 +0900

    [add][main] add main bootstrap code

--- packages/presentation/main/src/bootstrap/text2speech.ts
+++ packages/presentation/main/src/bootstrap/text2speech.ts
@@ -1,18 +1,19 @@
 import { DependencyContainer } from "tsyringe";
-import { initEngineAndKuromoji } from "presentation_core";
+import { Engine, initEngineAndKuromoji } from "presentation_core";
 import { credentials, VerifyOptions } from "grpc";
 import { MixerClient } from "sound-mixing-proto/index_grpc_pb";
 import { promises as fs } from "fs";
+import { IpadicFeatures, Tokenizer } from "kuromoji";
 export default async function initText2Speech(
   container: DependencyContainer
-): Promise<void> {
+): Promise<[Tokenizer<IpadicFeatures>, Engine]> {
   const grpcMixerClient = process.env["GUJ_MIXER_RPC_SERVER"]
     ? new MixerClient(
         process.env["GUJ_MIXER_RPC_SERVER"],
         await makeCredentials(process.env["GUJ_MIXER_KEYS"])
       )
     : undefined;
-  await initEngineAndKuromoji(container, grpcMixerClient);
+  return await initEngineAndKuromoji(container, grpcMixerClient);
 }
 async function makeCredentials(keys: string | undefined) {
   const options: VerifyOptions = {
