14c7a11b4af4996674c319dd0efcf0aa5f38b527
tignear <tignear+m@gmail.com>
2020-10-24 14:38:54 +0900

    [add][core] info commands

---
+++ packages/presentation/core/src/commands-v2/info/info.ts
@@ -0,0 +1,16 @@
+import { CommandBase } from "@guild-utils/command-base";
+import { Message } from "discord.js";
+import { EmbedWithExecutorType, executorFromMessage } from "protocol_util-djs";
+import { getLangType } from "../../util/get-lang";
+
+export class CommandInfo implements CommandBase {
+  constructor(
+    private readonly response: (lang: string) => EmbedWithExecutorType,
+    private readonly getLang: getLangType
+  ) {}
+  async run(message: Message): Promise<void> {
+    const lk = await this.getLang(message.guild?.id);
+
+    await message.sendEmbed(this.response(lk)(executorFromMessage(message)));
+  }
+}
