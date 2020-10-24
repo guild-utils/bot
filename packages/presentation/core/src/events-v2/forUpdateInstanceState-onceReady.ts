9ad455c746588964f4fb25b22f7499aa58bd8462
tignear <tignear+m@gmail.com>
2020-10-24 14:49:17 +0900

    [add][core] add new style events

---
+++ packages/presentation/core/src/events-v2/forUpdateInstanceState-onceReady.ts
@@ -0,0 +1,23 @@
+import { ColorResolvable, MessageEmbed, TextChannel } from "discord.js";
+import { Client } from "discord.js";
+import { InstanceState } from "../instanceState";
+import { GUJ_LAUNCH_CHANNEL } from "../bootstrap/env";
+
+export default function (
+  client: Client,
+  instanceState: InstanceState,
+  color: ColorResolvable
+): void {
+  async function f() {
+    const embed = new MessageEmbed();
+    embed.setTitle("Starting");
+    embed.addField("Current", instanceState.state);
+    embed.addField("New", "running");
+    embed.setColor(color);
+    const channel = await client.channels.fetch(GUJ_LAUNCH_CHANNEL);
+    await (channel as TextChannel).sendEmbed(embed);
+  }
+  client.once("ready", (): void => {
+    f().catch(console.log);
+  });
+}
